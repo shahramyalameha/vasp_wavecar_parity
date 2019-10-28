f84120524999b4b3adbb357d2b3c9323495e9567
WangDH <55190228+ooteki-teo@users.noreply.github.com>
2019-10-28 10:26:33 +0800

    add 2d version

--- main.f90
+++ main.f90
@@ -1,18 +1,32 @@
 program main
     implicit none
-    integer :: ierr
+    integer :: ierr, nparameter
+    character :: dims, darg
 
+    nparameter=iargc()
+    if ( nparameter.ge.1 ) then
+        call getarg(1, darg)
+        if ( darg.eq.'-d2' ) dims='d2'
+        write(*,*) "**************** Set system dim to 2D *******************"
+        write(*,*) "I assume your system lay in AB plane! POSCAR lattice looks like"
+        write(*,*) "A(1) A(2) 0"
+        write(*,*) "B(1) B(2) 0"
+        write(*,*) "0    0    C"
+    else
+        dims='d3'
+        write(*,*) "**************** Set system dim to 3D *******************"
+    end if
     ierr=0
     open(unit=19, file='GCOEFF.txt', status='old', action='read', iostat=ierr)
     close(unit=19)
     if ( ierr.eq.0 ) then
-        write(*,*) "'GCOEFF.txt' file exits, now calc parity."
-        call parity
+        write(*,*) "Datafile 'GCOEFF.txt' exits, now calc parity."
+        call parity(dims)
         write(*,*) "**************End of parity calculation!*********************"
-    else 
+    else
         write(*,*) "'GCOEFF.txt' file dosn't exit, now read WAVECAR."
         call wavetrans
-        call parity
+        call parity(dims)
         write(*,*) "**************End of parity calculation!*********************"
     end if
 end program main
